6ddd0edb56fd689521e13f2bfc504b3591dfa6cf
Princeton Ebanks <PEbanks@PIOJ.local>
2018-10-07 18:36:35 -0500

    Add GET endpoints for users and tasks

--- src/server/server.py
+++ src/server/server.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify, request;
+from flask import Flask, jsonify, request, render_template;
 import json
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
 
 # POST /user data:{user_id, lasntname, firstname,...}
 @app.route('/user', methods=['POST'])
@@ -27,22 +31,69 @@ def create_user():
 
 	return jsonify(new_user)
 
+@app.route('/user', methods=['GET'])
+def get_users():
+	with open('data/users.json', "r") as userFile:
+		data = json.load(userFile)
+
+	return jsonify({'users': data})
+
+
 # GET  /user/<string: user_id>
 @app.route('/user/<string:id>', methods=['GET'])
-def get_user(id)
+def get_user(id):
+
+	with open('data/users.json', "r") as userFile:
+		users = json.load(userFile)
+
+	for user in users:
+		if(user['user_id'] == id):
+			return jsonify(user)
+
+	return jsonify({'message': 'user not found'})
+
+
+	
 
 # PUT  /user/<string: user_id> data{}
 @app.route('/user/<string:id>', methods=['PUT'])
-def update_user(id);
+def update_user(id):
+	return jsonify('{}')
+
 
 # POST /task data:{task_id, title, ...}
 @app.route('/task', methods=['POST'])
 def create_task():
+	return jsonify('{}')
+
+
 
 # PUT  /task/<string: task_id>
-@app.route('/task/<string:id>')
+@app.route('/task/<string:id>', methods=['PUT'])
 def update_task(id):
+	return jsonify('{}')
 
+
+
+@app.route('/task', methods=['GET'])
+def get_tasks():
+
+	with open('data/tasks.json', "r") as taskFile:
+		tasks = json.load(taskFile)
+
+	return jsonify({'tasks': tasks})
+
+
+@app.route('/task/<string:id>', methods=['GET'])
+def get_task(id):
+	with open('data/tasks.json', "r") as taskFile:
+		tasks = json.load(taskFile)
+
+	for task in tasks:
+		if(task['task_id'] == id):
+			return jsonify(task)
+
+	return jsonify({'message': 'task not found'})
 
 
 
